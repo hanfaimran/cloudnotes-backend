6f5eb547126eb0b8e5cc3e1d3f849c64c544d2d3
Hanfa <hanfaimran1441@Gmail.com>
2026-05-08 16:49:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,17 +4,30 @@ from config import Config
 from extensions import db
 from routes.auth import auth_bp
 from routes.notes import notes_bp
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
-    CORS(app, supports_credentials=True, origins=[
-    "http://localhost:3000",
-    "http://127.0.0.1:5500",
-    "https://heroic-mousse-f1e162.netlify.app"
-    "https://stickynotesz.netlify.app"
-])
+    allowed_origins = [
+        "http://localhost:3000",
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://stickynotesz.netlify.app",
+        "https://heroic-mousse-f1e162.netlify.app",
+    ]
+
+    CORS(app,
+         supports_credentials=True,
+         origins=allowed_origins,
+         allow_headers=["Content-Type", "Authorization"],
+         methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"]
+    )
+
     db.init_app(app)
 
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
